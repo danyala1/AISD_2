6cac96a3f1c527d6cea7502811be34c700d5057b
danyala1 <d.mayasov@mail.ru>
2023-05-14 23:49:32 +0400

    Сортировки выбором и Шелла

--- AISD_2/main.cpp
+++ AISD_2/main.cpp
@@ -51,3 +51,56 @@ std::vector<int> create_vector(int N, int chose) {
 	return TMP;
 }
 
+stats sorting_choice(std::vector<int> Vector) {
+	auto start = high_resolution_clock::now();
+	bool needing_restart = false;
+	stats A;
+	int min = 0; // дл€ записи минимального значени€
+	int buf = 0; // дл€ обмена значени€ми 
+
+	/*********** Ќачало сортировки **************/
+	for (int i = 0; i < Vector.size(); i++)
+	{
+
+		min = i; // запомним номер текущей €чейки, как €чейки с минимальным значением
+		// в цикле найдем реальный номер €чейки с минимальным значением
+		for (int j = i + 1; j < Vector.size(); j++)
+			min = (Vector[j] < Vector[min]) ? j : min;
+		A.copy_count += 1;
+		A.comparison_count += 1;
+		// cделаем перестановку этого элемента, помен€в его местами с текущим
+		if (i != min)
+		{
+			buf = Vector[i];
+			Vector[i] = Vector[min];
+			Vector[min] = buf;
+			A.copy_count += 3;
+		}
+		A.comparison_count += 1;
+	}
+	auto stop = high_resolution_clock::now();
+	duration<double> duration = stop - start;
+	A.time = (double)duration.count();
+	return A;
+}
+stats sorting_shell(std::vector<int> Vector) {
+	auto start = high_resolution_clock::now();
+	stats A;
+	for (int s = Vector.size() / 2; s > 0; s /= 2) {
+		for (int i = s; i < Vector.size(); ++i) {
+			for (int j = i - s; j >= 0 && Vector[j] > Vector[j + s]; j -= s) {
+				int temp = Vector[j];
+				Vector[j] = Vector[j + s];
+				Vector[j + s] = temp;
+				A.comparison_count += 1;
+				A.copy_count += 3;
+			}
+		}
+	}
+	auto stop = high_resolution_clock::now();
+	duration<double> duration = stop - start;
+	A.time = (double)duration.count();
+
+	return A;
+}
+
